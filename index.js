ad18958ab62ca0434b9557d44904fc478812435c
dunxtand <mdrstandish@gmail.com>
2017-03-27 18:17:03 -0400

    minor changes

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ try {
   var constructor = window.HTMLFormElement || window.document.createElement("FORM").constructor;
   constructor.prototype.zendeskTicketForm = protoMethod;
 } catch (error) {
-  window.console.error(
-    "zendesk-ticket-form will not work in this browser!",
+  console.error(
+    "zendesk-ticket-form will not work in this environment!",
     "Cannot access HTMLFormElement prototype:",
     error
   );
